f183d8dfeb2ffdfaa0e22cedd80906ef9ada8df1
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-02-17 22:31:30 -0300

    FEATURE: Update Logo component to use full width and height for the logo image

--- resources/js/Components/Logo.jsx
+++ resources/js/Components/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "@inertiajs/react";
 export default function Logo() {
     return(
         <Link href="/">
-            <img src="/assets/logo_js.svg" className="h-24" />
+            <img src="/assets/logo_js.svg" className="w-full h-full" />
         </Link>
     );
 }
