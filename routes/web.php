58f623e17c153834f68256b06308baf96055132b
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-02-21 01:58:44 -0300

    FEATURE: Refactor routes to group PropertyPagesController with new search endpoints

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,21 @@
 
 use App\Http\Controllers\Pages\GuestPagesController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\Pages\PropertyPagesController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', [GuestPagesController::class, 'home']);
+
+Route::controller(GuestPagesController::class)->group(function(){
+    Route::get('/', [GuestPagesController::class, 'home']);
+});
+
+Route::controller(PropertyPagesController::class)->prefix('/imovel')->group(function(){
+    Route::get("/search","list");
+    Route::get("/search/{text}","search_property_by_name");
+    Route::get("/search/{type?}/{operation?}/{neighborhood?}/{max_value?}","search_property_by_fiters");
+    Route::get("/{encrypted_id}","property_description");
+});
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard/Dashboard');
