e17facb77956d87ce5f4927e0ad1136c88ed6b40
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-03-06 18:40:05 -0300

    FEATURE: Implement property listing and description methods in PropertyPagesController;

--- app/Http/Controllers/Pages/PropertyPagesController.php
+++ app/Http/Controllers/Pages/PropertyPagesController.php
@@ -4,28 +4,38 @@ namespace App\Http\Controllers\Pages;
 
 use App\Encryption;
 use App\Http\Controllers\Controller;
+use App\Models\Neighborhood;
 use App\Models\Property;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PropertyPagesController extends Controller
 {
-    public function list(){
+    public function list()
+    {
+        $properties = Property::with('main_photo')->latest()->get();
+        $neighborhoods = Neighborhood::orderBy('name', 'asc')->get();
 
-    }
-
-    public function search_property_by_name(string $search_text){
-
-    }
-
-    public function search_property_by_fiters(?string $type, ?string $operation, ?string $neighborhood,?int $max_value){
-
-    }
-
-    public function property_description(string $encrypted_id){
-        $property_id = Encryption::decrypt($encrypted_id);
-        $property = Property::find($property_id);
+        foreach ($properties as $property) {
+            $property->encryptId();
+        }
         
-        return Inertia::render("Property/PropertyPage",["property"=>$property]);
+        return Inertia::render('Property/PropertyList',
+        [
+            'properties' => $properties,
+            'neighborhoods' => $neighborhoods,
+        ]);
+    }
+    
+    public function search_property_by_name(string $search_text) {}
+    
+    public function search_property_by_fiters(?string $type, ?string $operation, ?string $neighborhood, ?int $max_value) {}
+    
+    public function property_description(string $encrypted_id)
+    {
+        $property_id = Encryption::decrypt($encrypted_id);
+        $data = Property::find($property_id);
+        $data->encryptId();
+
+        return Inertia::render('Property/PropertyPage', ['property' => $data->toArray()]);
     }
 }
