64bb2227b9a2543554897ca78f4bd71c43035bc8
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-03-07 09:03:49 -0300

    FIX: Update ActionButton component to use className instead of class

--- resources/js/Components/ActionButton.jsx
+++ resources/js/Components/ActionButton.jsx
@@ -1,6 +1,6 @@
 export default function ActionButton({ children }){
     return (
-        <button type="button" class="p-2 py-1 rounded text-lg font-regular bg-red-800 text-red-50 border-e text-center w-full active:bg-red-900 transition-colors duration-200 ease-in-out">
+        <button type="button" className="p-2 py-1 rounded text-lg font-regular bg-red-800 text-red-50 border-e text-center w-full active:bg-red-900 transition-colors duration-200 ease-in-out">
             {children}
         </button>
     );
