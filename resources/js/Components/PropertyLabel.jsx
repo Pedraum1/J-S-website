fea684b3e89ffbedb8c555bd406584ff35bb8d43
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-03-09 14:11:09 -0300

    STYLE: Update class attributes to className in JSX components

--- resources/js/Components/PropertyLabel.jsx
+++ resources/js/Components/PropertyLabel.jsx
@@ -2,22 +2,22 @@ import ActionButton from "./ActionButton";
 
 export default function PropertyLabel({ property_data = {} }) {
     return (
-        <tr class="bg-white border-b border-gray-200">
+        <tr className="bg-white border-b border-gray-200">
             <th
                 scope="row"
-                class="px-6 py-4 font-medium whitespace-nowrap dark:text-white"
+                className="px-6 py-4 font-medium whitespace-nowrap dark:text-white"
             >
                 Apple MacBook Pro 17"
             </th>
-            <td class="px-6 py-4">Rua Arruda Câmara 147</td>
-            <td class="px-6 py-4">Laptop</td>
-            <td class="px-6 py-4">R$ 2999,00</td>
-            <td class="px-6 py-4">
-                <div class="inline-flex rounded-md shadow-xs overflow-hidden" role="group">
-                    <ActionButton><i class="fa-regular fa-eye"></i></ActionButton>
-                    <ActionButton><i class="fa-regular fa-trash-can"></i></ActionButton>
-                    <ActionButton><i class="fa-regular fa-pen-to-square"></i></ActionButton>
-                    <ActionButton><i class="fa-regular fa-trash-can"></i></ActionButton>
+            <td className="px-6 py-4">Rua Arruda Câmara 147</td>
+            <td className="px-6 py-4">Laptop</td>
+            <td className="px-6 py-4">R$ 2999,00</td>
+            <td className="px-6 py-4">
+                <div className="inline-flex rounded-md shadow-xs overflow-hidden" role="group">
+                    <ActionButton><i className="fa-regular fa-eye"></i></ActionButton>
+                    <ActionButton><i className="fa-regular fa-trash-can"></i></ActionButton>
+                    <ActionButton><i className="fa-regular fa-pen-to-square"></i></ActionButton>
+                    <ActionButton><i className="fa-regular fa-trash-can"></i></ActionButton>
                 </div>
             </td>
         </tr>
--- resources/js/Pages/Dashboard/Dashboard.jsx
+++ resources/js/Pages/Dashboard/Dashboard.jsx
@@ -16,23 +16,23 @@ export default function Dashboard() {
             <section className="my-12 mx-auto max-w-7xl sm:p-6 lg:p-8 bg-white shadow-sm sm:rounded-lg">
                 <h2 className="text-3xl mb-4">Lista de imóveis</h2>
 
-                <div class="relative overflow-x-auto">
-                    <table class="w-full text-sm text-left rtl:text-right">
-                        <thead class="uppercase">
+                <div className="relative overflow-x-auto">
+                    <table className="w-full text-sm text-left rtl:text-right">
+                        <thead className="uppercase">
                             <tr>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" className="px-6 py-3">
                                     tipo
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" className="px-6 py-3">
                                     endereço
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" className="px-6 py-3">
                                     valor
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" className="px-6 py-3">
                                     ativo
                                 </th>
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" className="px-6 py-3">
                                     ações
                                 </th>
                             </tr>
