ff686d9d8de77bc31c0b6f9436e635da1db7df15
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-03-13 15:54:48 -0300

    FEATURE: Enhance ActionButton component to accept custom className for styling flexibility

--- resources/js/Components/ActionButton.jsx
+++ resources/js/Components/ActionButton.jsx
@@ -1,9 +1,9 @@
-export default function ActionButton({ children, action, type="button" }) {
+export default function ActionButton({ children, action, className = "", type="button" }) {
     return (
         <button
             type={type}
             onClick={action}
-            className="p-2 py-2 rounded font-regular bg-red-800 text-red-50 border-e text-center w-full active:bg-red-900 transition-colors duration-200 ease-in-out"
+            className={"p-2 py-2 rounded font-regular bg-red-800 text-red-50 border-e text-center w-full active:bg-red-900 transition-colors duration-200 ease-in-out " + className}
         >
             {children}
         </button>
