991465583b772524d9ec3050de0501ac08dccc31
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-03-14 14:25:01 -0300

    FEATURE: Applying Nunomaduro recommendations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +24,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureVite();
+    }
+
+    private function configureCommands(): void {
+        DB::prohibitDestructiveCommands(
+            App::environment('production')
+        );
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+        Model::unguard();
+    }
+
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
         Vite::prefetch(concurrency: 3);
     }
 }
