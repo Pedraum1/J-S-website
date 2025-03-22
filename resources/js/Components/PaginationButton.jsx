d10d8ef148db5a15f07ca5c9049175d935882569
Pedro Paulo <pedro.paulo60@aluno.ifce.edu.br>
2025-03-22 16:14:48 -0300

    FEATURE: Update PaginationButton to customize pagination text based on label

--- resources/js/Components/PaginationButton.jsx
+++ resources/js/Components/PaginationButton.jsx
@@ -1,12 +1,21 @@
 import { Link } from "@inertiajs/react";
 
 export default function PaginationButton({ link, arrayLength, currentPage }) {
+    console.log(link);
+
+    const getPaginationText = (label) => {
+        if(label == "pagination.previous"){return "&#x3c;"}
+        if(label == "pagination.next"){return "&#x3e;"}
+        return label;
+
+    }
+    
     return (
         <Link href={link.url}>
             <button
                 type="button"
                 className={"md:px-4 px-2 py-2 text-sm bg-red-800 hover:bg-red-900 transition-colors duration-200 ease-out text-red-50 " + (currentPage === (arrayLength-1) ? "rounded-r-lg " : "") + (currentPage === 0 ? "rounded-l-lg" : "")}
-                dangerouslySetInnerHTML={{ __html: link.label }}
+                dangerouslySetInnerHTML={{ __html: getPaginationText(link.label) }}
             >
             </button>
         </Link>
